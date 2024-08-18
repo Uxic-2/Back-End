7e09fbd6cf83e25cf0faccf753d1f555e6e6a3f0
SYEONIH <syeonih@naver.com>
2024-08-19 01:25:46 +0900

    로그인 로직 구현

--- photo_and_register/routes/member.js
+++ photo_and_register/routes/member.js
@@ -5,6 +5,22 @@ router.get('/login', (req, res) => {
     res.render('login.ejs');
 });
 
+// 로그인 데이터 처리
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    // 사용자가 입력한 이메일과 비밀번호로 데이터베이스에서 사용자 찾기
+    const user = await req.db.collection('login').findOne({ email, pw: password });
+
+    if (user) {
+        // 로그인 성공 시
+        res.status(200).send({ message: 'Login successful' });
+    } else {
+        // 로그인 실패 시
+        res.status(401).send({ message: 'Invalid email or password' });
+    }
+});
+
 // 회원가입 페이지 렌더링
 router.get('/register', (req, res) => {
     res.render('register.ejs');
@@ -30,7 +46,8 @@ router.post('/register', (req, res) => {
                 return res.status(500).send({ message: 'Database Insert Error' });
             }
             console.log('User registered:', result.insertedId);
-            res.status(200).send({ message: 'User registered successfully' });
+            console.log('회원가입을 축하드립니다');
+            res.status(200).send({ message: 'User registered successfully' }); // 상태 코드 200과 함께 응답
         }
     );
 });
